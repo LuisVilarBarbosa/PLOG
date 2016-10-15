ccc33ffb207d697187506b079a61e5bb131b5136
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2016-10-15 09:29:36 +0100

    Almost functional display

--- Nodes.pl
+++ Nodes.pl
@@ -23,16 +23,43 @@ board([
 /* Display */
 display_board(board) :-	/* board is not a variable so no question is asked */
 	board(Board), 
-	display_board_rows(Board).
+	display_board_rows(Board, Board).
 	
-display_board_rows([Row | []]) :-
+display_board_rows([Row | []], _Board) :-
+	display_board_middle_bottom_row(Row),
+	nl,
 	display_board_row_pieces(Row).
-display_board_rows([Row | Other_rows]) :-
+display_board_rows([Row | Other_rows], Board) :-	/* Display board up half */
+	length(Board, All_board_length),
+	length(Other_rows, Left_board_length),
+	Left_board_length1 is Left_board_length + 1,
+	Half_board_length is All_board_length / 2,
+	Left_board_length1 > Half_board_length,
 	display_board_row_pieces(Row), 
 	nl, 
 	display_board_middle_up_row(Row),
 	nl,
-	display_board_rows(Other_rows).
+	display_board_rows(Other_rows, Board).
+display_board_rows([Row | Other_rows], Board) :-	/* Display board middle */
+	length(Board, All_board_length),
+	length(Other_rows, Left_board_length),
+	Left_board_length1 is Left_board_length + 1,
+	Half_board_length is All_board_length / 2,
+	Left_board_length1 == Half_board_length,
+	display_board_row_pieces(Row), 
+	nl,
+	display_board_rows(Other_rows, Board).
+display_board_rows([Row | Other_rows], Board) :-	/* Display board bottom half */
+	length(Board, All_board_length),
+	length(Other_rows, Left_board_length),
+	Left_board_length1 is Left_board_length + 1,
+	Half_board_length is All_board_length / 2,
+	Left_board_length1 < Half_board_length,
+	display_board_middle_bottom_row(Row),
+	nl, 
+	display_board_row_pieces(Row), 
+	nl,
+	display_board_rows(Other_rows, Board).
 
 display_board_row_pieces([Piece | []]) :-
 	write(Piece).
@@ -45,7 +72,6 @@ display_board_row_pieces([Piece | Other_pieces]) :-
 	write(Piece),
 	write('-'),
 	display_board_row_pieces(Other_pieces).
-
 
 display_board_middle_up_row([Piece | []]):-
 	write('|').
@@ -66,6 +92,26 @@ display_board_middle_up_row([Piece | [' ' | Other_pieces]]) :-	%how it works?
 display_board_middle_up_row([Piece | Other_pieces]) :-
 	write('|X'),
 	display_board_middle_up_row(Other_pieces).
+
+display_board_middle_bottom_row([Piece | []]):-
+	write('|').
+display_board_middle_bottom_row([Piece | [' ']]):-
+	write('|/').
+display_board_middle_bottom_row([Piece | [Piece | ' ']]):-
+	write('|\\').
+display_board_middle_bottom_row([' ' | [Other_piece | Other_pieces]]) :-
+	Other_piece \== ' ',
+	write(' \\'),
+	display_board_middle_bottom_row(Other_pieces).
+display_board_middle_bottom_row([' ' | Other_pieces]) :-
+	write('  '),
+	display_board_middle_bottom_row(Other_pieces).
+display_board_middle_bottom_row([Piece | [' ' | Other_pieces]]) :-	%how it works? 
+	write('|X'),
+	display_board_middle_bottom_row(Other_pieces).
+display_board_middle_bottom_row([Piece | Other_pieces]) :-
+	write('|X'),
+	display_board_middle_bottom_row(Other_pieces).
 
 /* 
 recomendaçoes do prof: 
