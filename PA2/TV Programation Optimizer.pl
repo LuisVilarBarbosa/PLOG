f4ee6b0f9f229cfde393b76aa02356f466c49979
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2016-12-11 10:20:14 +0000

    Starting PA2 (originally initiated at 2016-12-05)

---
+++ PA2/TV Programation Optimizer.pl
@@ -0,0 +1,9 @@
+% The number of available series is always greater than the number of slots to fill.
+
+% series(Id, Name, Price, Duration, Restricted, DisplayableAnyTimesPerDay).	% Duration is multiple of 30
+% slot(SeriesId, Day, Hour, NumVotes).
+
+betterSol :-
+	findall(SeriesId, series(SeriesId, _, _, _, _, _), SeriesIds),
+	length(SeriesIds, Length),
+	length(L, Length).
