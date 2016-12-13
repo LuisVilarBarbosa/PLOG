40850870623eccd0add96afb4772f6ed92d3f11d
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2016-12-13 17:20:14 +0000

    Timer and modules added

--- PA2/TV Programation Optimizer.pl
+++ PA2/TV Programation Optimizer.pl
@@ -4,7 +4,20 @@
 % slot(Day, Hour).
 % preference(SeriesId, Day, Hour, NumVotes).
 
+:- use_module(library(clpfd)).
+:- use_module(data).
+
 betterSol :-
 	findall(SeriesId, series(SeriesId, _, _, _, _, _), SeriesIds),
 	length(SeriesIds, Length),
 	length(L, Length).
+	/*reset_timer,
+	labeling([], X),
+	print_time.*/
+
+
+reset_timer :- statistics(walltime,_).	
+print_time :-
+	statistics(walltime,[_,T]),
+	TS is ((T//10)*10)/1000,
+	nl, write('Time: '), write(TS), write('s'), nl, nl.
